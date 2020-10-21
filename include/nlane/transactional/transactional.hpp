1a981ab9e879ab9114ccce902df589857e473dca
Lorenzo Rai <lorenzo@rai-family.ch>
2020-10-21 02:14:35 +0200

    Added basic build environment

---
+++ include/nlane/transactional/transactional.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstddef>
+
+namespace nlane{
+namespace transactional {
+
+using word_t = uint64_t;
+
+} // namespace transactional
+
+namespace tr = transactional;
+} // namespace nlane
---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#include <nlane/transactional/transactional.hpp>
+
+#include <iostream>
+
+int main() {
+
+    nlane::tr::word_t t{ 0 };
+    std::cout << t << std::endl;
+
+    return 0;
+}
